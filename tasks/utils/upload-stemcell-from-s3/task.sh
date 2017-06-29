c5ae402c9e0eead312dd8ef0403a0e94c0386799
Luciano Silva <lsilva@pivotal.io>
2017-06-29 11:49:41 -0400

    Fix stemcell tasks 2

--- tasks/utils/upload-stemcell-from-s3/task.sh
+++ tasks/utils/upload-stemcell-from-s3/task.sh
@@ -19,9 +19,9 @@ s3BucketName=""
 [ -n "$S3_ENDPOINT" ] && s3EndPointUrl=$S3_ENDPOINT
 [ -z "$S3_ENDPOINT" ] && s3EndPointUrl="s3-$s3RegionName.amazonaws.com" # per http://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
 
-[ -z "$S3_DISABLE_SSL" ] && s3DisableSSLCheck="$S3_DISABLE_SSL"
-[ -z "$S3_V2" ] && s3v2Signing="$S3_V2"
-[ -z "$S3_REGION_NAME" ] && s3RegionName=$S3_REGION_NAME
+[ -n "$S3_DISABLE_SSL" ] && s3DisableSSLCheck="$S3_DISABLE_SSL"
+[ -n "$S3_V2" ] && s3v2Signing="$S3_V2"
+[ -n "$S3_REGION_NAME" ] && s3RegionName=$S3_REGION_NAME
 
 setS3CLI "$S3_ACCESS_KEY_ID" "$S3_SECRET_ACCESS_KEY" "$s3v2Signing"
 
